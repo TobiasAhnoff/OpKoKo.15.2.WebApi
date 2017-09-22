ade2ee6ac3ef6edc41897f20eb7c24b4330b2525
TobiasAhnoff <tobias.ahnoff@gmail.com>
2017-09-22 22:32:22 +0200

    Add bat-files fort certs

---
+++ OpKoKo.17.2.Core/OpKokoDemo/CreateAndImportAuthServerSigningCert.bat
@@ -0,0 +1,3 @@
+﻿rem From https://brockallen.com/2015/06/01/makecert-and-creating-ssl-or-signing-certificates/
+makecert -r -pe -n "CN=AuthorizationServer" -b 01/01/2017 -e 01/01/2027 -eku 1.3.6.1.5.5.7.3.3 -sky signature -a sha256 -len 2048 -ss my -sr LocalMachine
+pause
---
+++ OpKoKo.17.2.Core/OpKokoDemo/CreateAndImportLocalhostSSLCert.bat
@@ -0,0 +1,3 @@
+﻿rem From https://brockallen.com/2015/06/01/makecert-and-creating-ssl-or-signing-certificates/
+makecert -r -pe -n "CN=localhost" -b 01/01/2017 -e 01/01/2027 -eku 1.3.6.1.5.5.7.3.1 -sky exchange -a sha256 -len 2048 -ss my -sr localMachine
+pause
